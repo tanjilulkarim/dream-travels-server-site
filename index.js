feb49e79d6370d7aba45c88c9578760b15330676
tanjilulkarim <tanjilkarim580@gmail.com>
2021-10-31 18:06:26 +0600

    without delete and updateHero

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { json } = require('express');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
